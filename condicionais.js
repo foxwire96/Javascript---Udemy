b1c6e9272da2a0c509f418d1dc77e2655f991240
Milena Daflon <milenadaflon@gmail.com>
2024-01-16 14:47:08 -0300

    mais arquivos

---
+++ condicionais.js
@@ -0,0 +1,19 @@
+var a = 2;
+var b = 3;
+var c = "Olá mundo";
+
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
+if (c%2 == 1) {
+    console.log("Numero Impar");
+}
+else if (c%2 == 0){
+    console.log("Numero par");
+}
+
+else {
+    console.log("Valor Invalido");
+}
